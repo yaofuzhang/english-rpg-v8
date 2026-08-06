841931a701e8c4a2fa8a5c808902ab99ba71c050
大老板 <4163067.zhang@163.com>
2026-08-06 18:58:53 +0800

    v8.1 initial

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='erpg-v8';self.addEventListener('install',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.skipWaiting()))});self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
